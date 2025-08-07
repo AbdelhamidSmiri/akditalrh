cb3bfd878e11cc3065ae44c0b21f651e28bcbff8
Abdelhamid SMIRI <85844740+AbdelhamidSmiri@users.noreply.github.com>
2025-08-07 15:14:43 +0100

    end design conditions and email

--- app/Controller/BeneficiairesController.php
+++ app/Controller/BeneficiairesController.php
@@ -256,14 +256,15 @@ class BeneficiairesController extends AppController
 
 	function conditions($token)
 	{
+		$fin = "";
 		$this->set('token', $token);
 		$token = base64_decode(urldecode($token)) ^ 19051983;
 		$this->layout = "vide";
 		if ($this->request->is('post')) {
 			$this->Beneficiaire->id = $token;
 			$this->Beneficiaire->saveField("etat", "Valider");
-			echo $token;
-			exit();
+			// echo $token;
+			// exit();
 			$this->Session->setFlash(
 				'Votre engagement a été validé avec succès.',
 				'Flash/success',
@@ -273,9 +274,8 @@ class BeneficiairesController extends AppController
 
 
 			$fin = "ok";
-			$this->set('fin', $fin);
-			echo "abdhamid chouf f view dial conditions wach jayak fin= ok si oui affiche dak message dial figma votre....";
-			exit();
+			
 		}
+		$this->set('fin', $fin);
 	}
 }
